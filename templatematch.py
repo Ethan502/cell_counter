8ee868c3bdbe512b4b3b99ea262e80815c223ab9
Ethan502 <ethan.smith502@yahoo.com>
2021-06-18 16:17:22 -0600

    fixed the master

--- templatematch.py
+++ templatematch.py
@@ -5,6 +5,8 @@ def matcher(img_data, template):
 
     output = img_data.copy()
 
+    w,h = template.shape[::-1]
+
     if len(img_data.shape) >= 3:
         gray = cv.cvtColor(img_data, cv.COLOR_BGR2GRAY)
     else:
@@ -23,62 +25,12 @@ def matcher(img_data, template):
     whys = []
     exes = []
     for pt in zip(*loc[::-1]):
-<<<<<<< HEAD
-        x,y = pt
-        exes.append(x)
-        whys.append(y)
-
-    #make the indexes list of non-duplicate x-values
-    indexes = []
-    doubles = []
-    for i in exes:
-        if i not in doubles:
-            doubles.append(i)
-            temp = exes.index(i)
-            indexes.append(temp)
-
-    
-
-    #seperate find the correct x and y values at their indexes
-    new_xs =[]
-    for partx in indexes:
-       temp =  exes[partx]
-       new_xs.append(temp)
-
-    new_ys =[]
-    for party in indexes:
-       temp2 =  whys[party]
-       new_ys.append(temp2)
-
-    #stitch the points together into the array
-    newpts = []
-    for spot in range(0,len(indexes)):
-        x = new_xs[spot]
-        y = new_ys[spot]
-        newpt = x, y
-        newpts.append(newpt)
-
-    # counter = 0
-    # for part in newpts[::-1]:
-    #     cv.rectangle(output, part, (part[0] + w, part[1] + h), (0,0,0),1)
-    #     cv.circle(output,part,4,(0,0,255),2)
-    #     counter = counter + 1
-
-    # doc1 = open('old.txt', 'w')
-    # doc2 = open('new.txt', 'w')
-    
-    # doc1.write(str(newpts))
-    # doc2.write(str(new_xs))
-    
-    # cv.rectangle(output, pt, (pt[0] + w, pt[1] + h), (0,0,0), 1)
-
-    # output = cv.resize(output, (0,0), fx = 0.75, fy= 0.75)
-    # cv.imshow('window', output)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-=======
         cv.rectangle(output, pt, (pt[0] + w, pt[1] + h), (0,0,0), 1)
         #cv.circle(output, (pt[1],pt[0]), 5, (255,0,0), 1)
->>>>>>> c5496d73cfc074e54c412ba048dd0f9063f59118
 
-    return newpts
+    output = cv.resize(output, (0,0), fx = 0.5, fy= 0.5)
+    cv.imshow('cells', output)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    
